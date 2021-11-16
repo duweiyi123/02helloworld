b58485192006595d17a07dea0b0119584e1540df
duweiyi <1070514997@qq.com>
2021-11-16 10:26:09 +0800

    duweiyi first commit

---
+++ src/main/java/com/dwy/boot/duweiyi.java
@@ -0,0 +1,11 @@
+package com.dwy.boot;
+
+/**
+ * @author 杜Sir
+ * @create 2021-11-16 10:24
+ */
+public class duweiyi {
+    public static void main(String[] args) {
+        System.out.println("duwieyi");
+    }
+}
